e1082750a8012265f4fb568834c0b2059d4c37a1
Oda Schneider <oda.schneider@gmx.de>
2022-09-20 20:59:20 +0200

    startpage animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
 
   constructor(public router: Router) { }
+
+  ngOnInit(): void {
+    AOS.init();
+  }
 
 }
